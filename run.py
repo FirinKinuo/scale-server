be3c160e468784451aeddd3c78e62da2166e84b1
FirinKinuo <firinkinuo@protonmail.ch>
2021-06-16 17:58:36 +0300

    change to use 0.0.0.0 ip

--- run.py
+++ run.py
@@ -14,5 +14,5 @@ if __name__ == '__main__':
     com_serial = Serial(port=environ.get('COMPORT_PATH'), baudrate=environ.get('COMPORT_BAUDRATE'))
 
     # Создаем экземпляр класса для работы c Web
-    web = Web(serial=com_serial, host=environ.get('WEB_HOST'), port=int(environ.get('WEB_PORT')))
+    web = Web(serial=com_serial, host=environ.get('0.0.0.0'), port=int(environ.get('WEB_PORT')))
     web.start()  # Запускаем сервер для обработки данных

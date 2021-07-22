224bcd866f16614f834bef11e7bcb000881b240c
FirinKinuo <firinkinuo@protonmail.ch>
2021-07-22 19:24:57 +0300

    fix jinja template dir missing

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from cx_Freeze import setup, Executable
 import sys
-
+from sys import path as sys_path
+from os import path
 
 executables = [Executable('run.py', targetName='weight_comport.exe')]
 
@@ -13,13 +14,15 @@ zip_include_packages = ["aiohttp", "aiohttp_jinja2", "app", "async_timeout", "as
                         "distutils", "dotenv", "email", "encodings", "html",
                         "http", "idna", "importlib", "jinja2", "json",
                         "lib2to3", "logging", "markupsafe", "multidict", "multiprocessing",
-                        "pydoc_data", "serial", "test", "tkinter", "unittest",
-                        "urllib", "xml", "xmlrpc", "yarl"]
+                        "pydoc_data", "serial", "urllib", "xml", "xmlrpc", "yarl"]
+
+include_dirs = ['templates/']
 
 options = {
     'build_exe': {
         'include_msvcr': True,
         'excludes': excludes,
+        'include_files': include_dirs,
         'includes': includes,
         'zip_include_packages': zip_include_packages,
         'build_exe': 'build_windows',
@@ -32,7 +35,7 @@ if sys.platform.startswith('linux'):
     options['build_exe']['build_exe'] = 'build_linux'
 
 setup(name='weight_comport',
-      version='1.0.2',
+      version='1.0.4',
       description='Чтение компорта промышленных весов с отправкой данных по webhook и на табло по компорту',
       executables=executables,
       options=options)

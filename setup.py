0fa326bb7f7896019737bd2a69592a8ec8b68a37
FirinKinuo <deals@fkinuo.ru>
2022-04-12 15:52:16 +0300

    Update: Packages versions, split dependencies into production and dev

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from pkg_resources import parse_requirements
 from setuptools import setup
 
 NAME = "Scale Server"
-VERSION = '2.4.1'
+VERSION = '2.4.2'
 DESCRIPTION = 'Handler of weights of various companies for exchange with 1C by HTTP'
 MODULES = ['app', 'app.web', 'app.scales', 'app.scales.ethernet', 'app.scales.ethernet.massak',
            'app.scales.scale_serial', 'app.settings']
 
 
 def load_requirements(filename: str) -> list:
-    with open(filename, 'r', encoding="utf-8") as file:
+    with open(file=filename, mode='r', encoding="utf-8") as file:
         return [f"""{req.name}{f"[{','.join(req.extras)}]" if req.extras else ''}{req.specifier}"""
                 for req in parse_requirements(file.read())]
 
@@ -18,12 +18,12 @@ setup(
     name=NAME,
     version=VERSION,
     packages=MODULES,
-    url='https://github.com/FirinKinuo/weight-comport-processing',
+    url='https://github.com/FirinKinuo/scale-server',
     license='GPL-3.0',
     author='fkinuo',
     author_email='deals@fkinuo.ru',
     description=DESCRIPTION,
-    install_requires=load_requirements('requirements.txt'),
+    install_requires=load_requirements(filename='requirements.txt'),
     entry_points={
         'console_scripts': [
             'scale_server = app.__main__',

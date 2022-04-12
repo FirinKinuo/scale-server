b6ef9089060c226f1f38076f3b942214ed60c2fa
FirinKinuo <deals@fkinuo.ru>
2022-04-12 16:23:49 +0300

    Update: Display information about starting the server

--- app/__main__.py
+++ app/__main__.py
@@ -13,7 +13,10 @@ if platform.startswith('win32') or platform.startswith('cygwin'):
 
 # Старт сервиса
 if __name__.endswith('__main__'):
-    log.info("Starting Scale Server | https://git.fkinuo.ru/scale-server")
+    log.info("Starting...")
+    print(f"Starting Scale Server || "
+          f"transmission: {web.config.TRANSMISSION_HOST}:{web.config.TRANSMISSION_PORT} | "
+          f"visual: {web.config.VISUAL_HOST}:{web.config.VISUAL_PORT}")
 
     list(map(lambda serial: serial.connect(), SCALES + OUTPUTS))  # Инициализация подключения к весам и выводу
 
